28213545f656246ca239fe4fc22263c873a3318a
Hilol <imanuilyusupov@gmail.com>
2024-03-08 14:40:23 +0300

    скрипт

--- create.py
+++ create.py
@@ -7,5 +7,4 @@ directory = "C:/Users/HP-PC/Desktop/study/4 семестр/вэб/лаб2"  # У
 for i in range(1, 25):
     # вместо слова primer прописываешь название своего файла которое нужно создать вместо html расширение которое тебе нужно
     file_path = os.path.join(directory, f"primer{i}.html")
-    with open(file_path, "w") as file:
-        file.write("<!DOCTYPE html>\n<html>\n<head>\n<title>File Title</title>\n</head>\n<body>\n<h1>File Content</h1>\n</body>\n</html>")
+
